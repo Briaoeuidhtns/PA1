24a45ea94ceca6cdbe1389cdf3e98d4ab2298eab
Brian Brunner <brian.brunner@mines.sdsmt.edu>
2019-02-27 11:31:46 -0700

    delete extra files

--- populate_Tables.sql
+++
@@ -1,7 +0,0 @@
-EXEC 'populate_Driver.sql';
-EXEC 'populate_Tollway.sql';
-EXEC 'populate_TollGate.sql';
-EXEC 'populate_Vehicle.sql';
-EXEC 'populate_ETCReading.sql';
-EXEC 'populate_Constructor.sql';
-EXEC 'populate_Constructed.sql';
